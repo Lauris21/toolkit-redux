d9791ed4fd6a88d99dda75f431302038f805a63d
Laura <laurasanchez0301@gmail.com>
2023-11-27 20:37:59 +0100

    RTK query

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
-import { PokemonApp } from "./PokemonApp.jsx";
+import { TodoApp } from "./TodoApp";
+// import { PokemonApp } from "./PokemonApp.jsx";
 
 // Proveemos el store a la app
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       {/* <App /> */}
-      <PokemonApp />
+      {/* <PokemonApp /> */}
+      <TodoApp />
     </Provider>
   </React.StrictMode>
 );
